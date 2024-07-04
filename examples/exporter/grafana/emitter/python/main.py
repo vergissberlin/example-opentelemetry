1d10fb099cba889f8cdf40e402a36b552792151d
André Lademann <alademann@thinkport.digital>
2024-07-04 23:13:42 +0200

    feat(grafana/emitter/python): add a new Prometheus counter metric to track function calls

--- examples/exporter/grafana/emitter/python/main.py
+++ examples/exporter/grafana/emitter/python/main.py
@@ -1,9 +1,9 @@
-from prometheus_client import start_http_server, Summary
+from prometheus_client import start_http_server, Summary, Counter
 import random
 import time
 
 REQUEST_TIME = Summary("request_processing_seconds", "Time spent processing a function")
-
+MY_COUNTER= Counter("emitter:python:counter", "A counter for my function", ["mylabel"])
 @REQUEST_TIME.time()
 def process_request(t):
     """A dummy function that takes some time."""
@@ -12,6 +12,7 @@ def process_request(t):
 if __name__ == '__main__':
     start_http_server(5000)
     while True:
+        MY_COUNTER.labels(mylabel='myvalue').inc()
         process_request(random.random())
         #push_to_gateway('localhost:9090', job='myjob', registry=CollectorRegistry())
         #time.sleep(1)
