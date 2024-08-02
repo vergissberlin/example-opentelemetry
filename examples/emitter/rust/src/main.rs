5c2ae944f99465a543b89d35c43e5b57a9f98f14
Tobias Hau <thau@thinkport.digital>
2024-08-02 18:35:58 +0200

    fixup! WIP: feature(emitter/rust): OTel rust example
    Add Instrumentation

--- examples/emitter/rust/src/main.rs
+++ examples/emitter/rust/src/main.rs
@@ -1,10 +1,21 @@
 use hyper::service::{make_service_fn, service_fn};
-use hyper::{Body, Request, Response, Server, Method, StatusCode};
+use hyper::{Body, Method, Request, Response, Server, StatusCode};
+use opentelemetry::trace::{SpanKind, Status};
+use opentelemetry::{global, trace::Tracer};
+use opentelemetry_sdk::propagation::TraceContextPropagator;
+use opentelemetry_sdk::trace::TracerProvider;
+use opentelemetry_stdout::SpanExporter;
 use rand::Rng;
 use std::{convert::Infallible, net::SocketAddr};
 
 async fn handle(req: Request<Body>) -> Result<Response<Body>, Infallible> {
     let mut response = Response::new(Body::empty());
+
+    let tracer = global::tracer("dice_server");
+
+    let span_builder = tracer
+        .span_builder(format!("{} {}", req.method(), req.uri().path()))
+        .with_kind(SpanKind::Server);
 
     match (req.method(), req.uri().path()) {
         (&Method::GET, "/rolldice") => {
@@ -13,14 +24,26 @@ async fn handle(req: Request<Body>) -> Result<Response<Body>, Infallible> {
         }
         _ => {
             *response.status_mut() = StatusCode::NOT_FOUND;
+            span_builder
+                .with_status(Status::error("Not found"))
+                .start(&tracer);
         }
     };
 
     Ok(response)
 }
 
+fn init_tracer() {
+    global::set_text_map_propagator(TraceContextPropagator::new());
+    let provider = TracerProvider::builder()
+        .with_simple_exporter(SpanExporter::default())
+        .build();
+    global::set_tracer_provider(provider);
+}
+
 #[tokio::main]
 async fn main() {
+    init_tracer();
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
 
     let make_svc = make_service_fn(|_conn| async { Ok::<_, Infallible>(service_fn(handle)) });
