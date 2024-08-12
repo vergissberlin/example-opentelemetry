2d60a9fb456d013cb92899eadf60b4ab262fddfe
André Lademann <alademann@thinkport.digital>
2024-08-12 18:58:25 +0200

    feat: Update Makefile and related helper files for project automation

--- Make/application.mk
+++ Make/application.mk
@@ -3,42 +3,6 @@
 # TARGETS
 # =============================================================================
 
-PATH_COLLECTORS=examples/collectors
-PATH_RECEIVERS=examples/receivers
-PATH_EMITTERS=examples/emitters
-PATH_EXPORTERS=examples/exporters
-
-LIST_COLLECTORS=opentelemetry
-LIST_EMITTERS=nodejs
-LIST_RECEIVERS=prometheus
-LIST_EXPORTERS=
-
-
-define SERVICE_STATUS
-	@echo -e "${H1BEGIN}${1}${H1END}"
-	@for item in ${2}; do \
-		if [ $$(docker compose -f ${3}/$${item}/compose.yml ps --services --status running | wc -l) -gt 0 ]; then \
-			echo -e "${PBEGIN}Running ${1} in $${item}${PEND}"; \
-			echo -e "${P_SUCCESS_BEGIN}"; \
-			docker compose -f ${3}/$${item}/compose.yml ps --services --status running; \
-			echo -e "${P_SUCCESS_END}"; \
-		fi; \
-		if [ $$(docker compose -f ${3}/$${item}/compose.yml ps --services  --status exited --status paused --status restarting --status removing --status dead --status exited | wc -l) -gt 0 ]; then \
-			echo -e "${PBEGIN}Failing ${item} in $${item}${PEND}"; \
-			echo -e "${P_WARNING_BEGIN}"; \
-			docker compose -f ${3}/$${item}/compose.yml ps --services --status exited --status paused --status restarting --status removing --status dead --status exited]; \
-			echo -e "${P_WARNING_END}"; \
-		fi; \
-	done
-endef
-
-define SERVICE_CONTROL
-	@echo -e "${H1BEGIN}${1}${H1END}"
-	@for item in ${2}; do \
-		docker compose -f ${3}/$${item}/compose.yml ${4}; \
-	done
-endef
-
 os: .logo ## Show running containers
 	$(call DOCKER_SERVICE,Emitter,${LIST_EMITTERS},${PATH_EMITTERS})
 
--- Make/collector.mk
+++ Make/collector.mk
@@ -1,26 +1,25 @@
-COLLECTOR_COMPOSE_FILE=-f iac/docker/compose.yml
-
 # =============================================================================
 # TARGETS
 # =============================================================================
 
 #### Collector
-collector-up: .logo ## Starts the opentelemetry collector
-	 docker compose ${COLLECTOR_COMPOSE_FILE} up -d
+collector-up: .logo ## Starts the collectors
+	$(call SERVICE_CONTROL,Collectors,${LIST_COLLECTORS},${PATH_COLLECTORS},up -d)
 
-collector-down: .logo ## Starts the opentelemetry collector
-	 docker compose ${COLLECTOR_COMPOSE_FILE} down -v
+collector-down: .logo ## Shuts down the collectors
+	$(call SERVICE_CONTROL,Collectors,${LIST_COLLECTORS},${PATH_COLLECTORS},down -v)
 
-collector-status: .logo ## Show running containers
-	@docker compose ${COLLECTOR_COMPOSE_FILE} ps
+collector-status: .logo ## Show the status of the collectors
+	$(call SERVICE_STATUS,Collectors,${LIST_COLLECTORS},${PATH_COLLECTORS})
 
-collector-logs: .logo ## Show logs from containers
-	 docker compose ${COLLECTOR_COMPOSE_FILE} logs -f
+collector-logs: .logo ## Show logs from the collectors
+	$(call SERVICE_CONTROL,Collectors,${LIST_COLLECTORS},${PATH_COLLECTORS},logs -f)
 
 collector-open: .logo ## Open the collector in the browser
 	@echo -e "${H2BEGIN}Open the collector in the browser${H2END}"
 	@echo -e "${PBEGIN}open http://0.0.0.0:16686${PEND}\n"
 	@open http://0.0.0.0:16686
+
 # =============================================================================
 # TARGET ALIASES
 # =============================================================================
