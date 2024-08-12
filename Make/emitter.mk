7a92ee24e34cab4b463aa28b1e786626ad60e200
André Lademann <alademann@thinkport.digital>
2024-08-12 19:07:25 +0200

    refactor(emitter.mk): use PATH_EMITTERS variable for emitter paths to improve maintainability and flexibility

--- Make/emitter.mk
+++ Make/emitter.mk
@@ -6,28 +6,28 @@
 emitter-%-up: .logo ## Starts the emitter in the specified language
 	@make .setup
 	@echo -e "${H2BEGIN}Run emitter with ${*}${H2END}"
-	@echo -e "${PBEGIN}docker compose -f examples/emitter/$*/compose.yml up -d${PEND}\n"
-	@docker compose -f examples/emitter/$*/compose.yml up -d
+	@echo -e "${PBEGIN}docker compose -f ${PATH_EMITTERS}/$*/compose.yml up -d --build${PEND}\n"
+	@docker compose -f ${PATH_EMITTERS}/$*/compose.yml up -d --build
 
 emitter-%-restart: .logo ## Restarts the emitter in the specified language
 	@echo -e "${H2BEGIN}Restart emitter with ${*}${H2END}"
-	@echo -e "${PBEGIN}docker compose -f examples/emitter/$*/compose.yml down -v${PEND}\n"
-	@docker compose -f examples/emitter/$*/compose.yml down -v
-	@echo -e "${PBEGIN}docker compose -f examples/emitter/$*/compose.yml up -d${PEND}\n"
-	@docker compose -f examples/emitter/$*/compose.yml up -d
+	@echo -e "${PBEGIN}docker compose -f ${PATH_EMITTERS}/$*/compose.yml down -v${PEND}\n"
+	@docker compose -f ${PATH_EMITTERS}/$*/compose.yml down -v
+	@echo -e "${PBEGIN}docker compose -f ${PATH_EMITTERS}/$*/compose.yml up -d${PEND}\n"
+	@docker compose -f ${PATH_EMITTERS}/$*/compose.yml up -d
 
 emitter-%-down: .logo ## Stops the emitter in the specified language
 	@echo -e "${H2BEGIN}Stop emitter with ${*}${H2END}"
-	@echo -e "${PBEGIN}docker compose -f examples/emitter/$*/compose.yml down -v${PEND}\n"
-	@docker compose -f examples/emitter/$*/compose.yml down -v
+	@echo -e "${PBEGIN}docker compose -f ${PATH_EMITTERS}/$*/compose.yml down -v${PEND}\n"
+	@docker compose -f ${PATH_EMITTERS}/$*/compose.yml down -v
 
 emitter-%-ps: .logo ## Show the status of the emitter in the specified language
 	@echo -e "${H2BEGIN}Emitter status of ${*}${H2END}\n"
-	@docker compose -f examples/emitter/$*/compose.yml ps
+	@docker compose -f ${PATH_EMITTERS}/$*/compose.yml ps
 
 emitter-%-logs: .logo ## Show logs from the emitter in the specified language
 	@echo -e "${H2BEGIN}Emitter logs of ${*}${H2END}\n"
-	docker compose -f examples/emitter/$*/compose.yml logs -f
+	docker compose -f ${PATH_EMITTERS}/$*/compose.yml logs -f
 
 
 # =============================================================================
