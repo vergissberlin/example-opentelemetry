e713259d43bb272d8467f415db736c8817b49400
André Lademann <alademann@thinkport.digital>
2024-08-19 15:54:17 +0200

    refactor(app.ts): change endpoint from /rolldice to root (/) for simplicity

--- examples/emitters/nodejs/app.ts
+++ examples/emitters/nodejs/app.ts
@@ -5,7 +5,7 @@ import { rollTheDice } from './dice'
 const PORT: number = parseInt(process.env.PORT || '3030')
 const app: Express = express()
 
-app.get('/rolldice', (req, res) => {
+app.get('/', (req, res) => {
     const rolls = req.query.rolls ? parseInt(req.query.rolls.toString()) : NaN
     if (isNaN(rolls)) {
         res
@@ -17,5 +17,5 @@ app.get('/rolldice', (req, res) => {
 })
 
 app.listen(PORT, () => {
-    console.log(`Listening for requests on http://localhost:${PORT}/rolldice?rolls=12`)
+    console.log(`Listening for requests on http://localhost:${PORT}?rolls=12`)
 })
