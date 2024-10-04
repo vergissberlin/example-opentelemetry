71e9574c0b307ff4588c6d257c76360fea63b3a2
André Lademann <alademann@thinkport.digital>
2024-10-04 14:50:17 +0200

    feat(collector.mk): add collector restart target and update comments

--- Make/collector.mk
+++ Make/collector.mk
@@ -17,13 +17,20 @@ collectors-logs: .logo ## Show logs from the collectors
 	$(call SERVICE_CONTROL,Collectors,${LIST_COLLECTORS},${PATH_COLLECTORS},logs -f)
 
 #### Collectors (datadog, opentelemetry, opentelemetry-contrib)
-collector-%-up: .logo ## Starts the emitter in the specified language
+collector-%-up: .logo ## Starts the collector in the specified language
 	@make .setup
 	@echo -e "${H2BEGIN}Run collector with ${*}${H2END}"
 	@echo -e "${PBEGIN}docker compose -f ${PATH_COLLECTORS}/$*/compose.yml up
 	 -d --build --remove-orphans${PEND}\n"
 	@docker compose -f ${PATH_COLLECTORS}/$*/compose.yml up -d --build
 
+
+collector-%-restart: .logo ## Restarts the collector in the specified language
+	@echo -e "${H2BEGIN}Restart collector with ${*}${H2END}"
+	@echo -e "${PBEGIN}docker compose -f ${PATH_COLLECTORS}/$*/compose.yml down -v${PEND}\n"
+	@docker compose -f ${PATH_COLLECTORS}/$*/compose.yml down -v
+	@echo -e "${PBEGIN}docker compose -f ${PATH_COLLECTORS}/$*/compose.yml up -d --build --remove-orphans${PEND}\n"
+	@docker comp
 
 # =============================================================================
 # TARGET ALIASES
